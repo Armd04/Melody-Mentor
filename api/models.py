6e116b7c19e64bca4206c8d001816f698a9d9a6b
Alireza <mohaghegh.ar82@gmail.com>
2023-07-29 16:27:27 +0330

    course and video added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import Profile
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100, default='')
+    description = models.TextField()
+    allowed_users = models.ManyToManyField(Profile, related_name='allowed_courses')
+
+    def __str__(self):
+        return self.title
+    
+class Video(models.Model):
+    title = models.CharField(max_length=100, default='')
+    description = models.TextField()
+    video_file = models.FileField(upload_to='videos/', null=True)
+    related_course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='related_videos')
+
+
+    def __str__(self):
+        return self.title
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import CourseSerializer, VideoSerializer
+from .models import Course, Video
+from rest_framework.views import APIView
+from rest_framework.response import responses
 
-# Create your views here.
+class CreateCourseView(generics.CreateAPIView):
+    model = Course
+    serializer_class = CourseSerializer
+
+class CreateVideoView(generics.CreateAPIView):
+    model = Video
+    serializer_class = VideoSerializer
+
+
+
